9e089a9e52588fcce7f0623eaede1d72ce9b425c
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2020-02-01 15:57:25 -0300

    Estilização da autenticação

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.svg';
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <>
+      <img src={logo} alt="logo-GoBarber" />
+      <form>
+        <input placeholder="Digite seu nome completo" />
+        <input type="email" placeholder="Digite seu email" />
+        <input type="password" placeholder="Digite sua senha" />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
