82155c99ef6683530759ec071f1a48d0e4e2d1af
WashingtonlGarcia <washingtonlgarcia@gmail.com>
2020-02-01 16:10:03 -0300

    Utilizando Unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.svg';
-// import { Container } from './styles';
 
 export default function SignIn() {
+  function handleSubmint(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="logo-GoBarber" />
-      <form>
-        <input type="email" placeholder="Seu email" />
-        <input type="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSubmint}>
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Sua senha" />
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
